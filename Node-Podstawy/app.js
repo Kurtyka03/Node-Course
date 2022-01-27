0fc786b69026f701bd89242ac78c924334cedf5a
Karol Kurtyka <kurtyka03@gmail.com>
2022-01-27 10:46:15 +0100

    .

--- Node-Podstawy/app.js
+++ Node-Podstawy/app.js
@@ -1,36 +1,7 @@
-const { readFile, writeFile } = require('fs').promises
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+const { createReadStream } = require('fs')
 
-const start = async () => {
-  try {
-    const first = await readFile('./content/firest.txt', 'utf8');
-    const second = await readFile('./content/secend.txt', 'utf8');
-    await writeFile(
-      './content/wynik.txt',
-      `THIS IS AWESOME : ${first} ${second}`,
-      { flag: 'a' }
-    )
-    console.log(first, second)
-  } catch (error) {
-    console.log(error)
-  }
-}
+const stream = createReadStream('./content/big.txt')
 
-start()
-
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, 'utf8', (err, data) => {
-//       if (err) {
-//         reject(err)
-//       } else {
-//         resolve(data)
-//       }
-//     })
-//   })
-// }
-// getText('./content/first.txt')
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err))
+stream.on('data', (result) => {
+  console.log(result);
+})
