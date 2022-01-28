6d478faf1b3f36bc85538b3ec1fe7908cb211000
Karol Kurtyka <kurtyka03@gmail.com>
2022-01-28 10:14:14 +0100

    .

--- Express-Podstawy/app.js
+++ Express-Podstawy/app.js
@@ -2,9 +2,22 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     res.writeHead(200,{'content-type': 'text/html'})
-    console.log(req.url);
-    res.write('<h1>home Page</h1>')
-    res.end()
+    const url = req.url
+    if(url === '/'){
+        console.log(req.url);
+        res.write('<h1>home Page</h1>')
+        res.end()
+    }
+    else if(url === '/about'){
+        console.log(req.url);
+        res.write('<h1>about Page</h1>')
+        res.end()
+    }
+    else{
+        console.log(req.url);
+        res.write('<h1>Page dosnt exist</h1>')
+        res.end()
+    }
 })
 
 server.listen(5000)
