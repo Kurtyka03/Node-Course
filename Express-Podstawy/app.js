c8c365569e8a22a1d2b1e14e55720f8ddf4d3bde
Karol Kurtyka <kurtyka03@gmail.com>
2022-01-28 10:11:10 +0100

    .

--- Express-Podstawy/app.js
+++ Express-Podstawy/app.js
@@ -1,1 +1,10 @@
-import { Express } from "express";
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200,{'content-type': 'text/html'})
+    console.log(req.url);
+    res.write('<h1>home Page</h1>')
+    res.end()
+})
+
+server.listen(5000)
